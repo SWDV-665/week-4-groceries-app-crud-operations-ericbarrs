e512fbd1008102b9d842685b39f5a080bfc6e4b8
Eric <yellastar42@yahoo.com>
2021-04-12 00:47:33 -0500

    week4 updates

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,146 @@
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { from } from 'rxjs';
+import {GroceriesServiceService} from '../groceries-service.service'
+
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  title = "Grocery List"
+  items = []
+  
+  constructor(
+    public toastController: ToastController, 
+    public alertController: AlertController,
+    public dataService: GroceriesServiceService
+  ){
+    this.items = dataService.items
+  }
+
+
+
+  async prompted (message:string){
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async addQuantity(item:any, index:number){
+
+    const Item = this.dataService.items[index]
+    
+    if(Item.quantity > 50){
+      this.prompted(item.name + ' has reached max quantity')
+      }
+
+      Item.quantity += 1
+      this.prompted(item.name + ' quantity is ' +  Item.quantity)
+  }
+
+  async removeItem(item:any, index:number){
+    const indexIem = this.dataService.items[index]
+    const Item = this.dataService.items
+
+    if(indexIem.quantity === 1){
+      this.prompted(item.name + ' has been removed.')
+      Item.splice(index,1)
+    }
+    else{
+      this.prompted(item.name + ' quantity has decreased')
+      indexIem.quantity -= 1
+    }
+  }
+
+  async editItem(index){
+    let item = this.dataService.items[index]
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Edit Item',
+      subHeader: 'New Item',
+      message: 'Edit a Shopping Item.',
+      inputs:[
+        {
+        name:'name',
+        placeholder:"Name",
+        value: item.name
+        },
+        {
+          name:'quantity',
+          placeholder:'Quantity',
+          value: item.quantity
+        }
+    ],
+      buttons:[
+        {
+        text:"Cancel",
+          handler: async () =>{
+            this.prompted('Cancelled')
+          }
+        },
+        {
+          text:"Save",
+            handler: async (newItem) =>{
+              item = newItem
+              this.prompted(item.name + ' has been updated.')
+            }
+        }
+      ]
+
+    });
+
+    await alert.present();
+  }
+
+  async deleteItem(index){
+    const Item = this.dataService.items
+
+    const deletedItem:any = Item.splice(index,1)
+    this.prompted(deletedItem[0].name + ' has been has been deleted.')
+  }
+
+  async addAlert() {
+    
+    let item = this.dataService.items
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Add Item',
+      subHeader: 'New Item',
+      message: 'Add a Shopping Item.',
+      inputs:[{
+        name:'Item',
+        placeholder:"add Item"
+      }],
+      buttons:[
+        {
+        text:"Cancel",
+          handler: async () =>{
+            this.prompted('Cancelled')
+          }
+        },
+        {
+          text:"Add",
+            handler: async (newItem) =>{
+              let obj ={
+                name:newItem.Item,
+                quantity:1
+              }
+              item.unshift(obj)
+              this.prompted(obj.name + ' has been added.')
+            }
+        }
+      ]
+
+    });
+
+    await alert.present();
+  }
+}
